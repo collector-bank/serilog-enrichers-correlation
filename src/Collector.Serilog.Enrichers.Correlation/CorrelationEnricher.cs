3b4e6bc2547e840f9ae7f5574bfad43c69687e85
Adam Lith <adam.lith@gmail.com>
2017-06-16 16:18:54 +0200

    Initial Commit

---
+++ src/Collector.Serilog.Enrichers.Correlation/CorrelationEnricher.cs
@@ -0,0 +1,25 @@
+﻿namespace Collector.Serilog.Enrichers.Correlation
+{
+    using System.Linq;
+
+    using Collector.Common.Correlation;
+
+    using global::Serilog.Core;
+    using global::Serilog.Events;
+
+    public class CorrelationEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var currentCorrelationId = CorrelationState.GetCurrentCorrelationId();
+            if (currentCorrelationId.HasValue)
+            {
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("CorrelationId", currentCorrelationId.Value));
+
+                var correlationDictionary = CorrelationState.GetCorrelationValues().ToDictionary(c => c.Key, c => c.Value);
+                if (correlationDictionary.Any())
+                    logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Correlation", correlationDictionary, destructureObjects: true));
+            }
+        }
+    }
+}
